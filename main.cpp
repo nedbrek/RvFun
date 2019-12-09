958aa46b676b34877e303f9f7404a775fbd79fcb
Edward Brekelbaum <nedbrek@yahoo.com>
2019-12-09 13:40:23 -0600

    Started architected state    Added 'num' to ArchState::getReg() and setReg()    Added PC functions (get and inc)    Added class SimpleArchState
       Implemented CompLI::execute()
    
       Changed execute loop to use PC from state
       Added PC to dump
       Dump state after run
    
    Added dox

--- main.cpp
+++ main.cpp
@@ -2,17 +2,64 @@
 #include <sstream>
 #include <iomanip>
 
+/// Interface to Architected State
 class ArchState
 {
 public:
-	virtual uint64_t getReg() const = 0;
-	virtual uint64_t setReg() const = 0;
+	virtual uint64_t getReg(uint32_t num) const = 0;
+	virtual void     setReg(uint32_t num, uint64_t val) = 0;
+
+	/// update PC
+	virtual void incPc(int64_t delta = 4) = 0;
+
+	/// get PC
+	virtual uint64_t getPc() const = 0;
 };
 
+/// Simple Implementation of ArchState
+class SimpleArchState : public ArchState
+{
+public:
+	SimpleArchState()
+	{
+	}
+
+	uint64_t getReg(uint32_t num) const override
+	{
+		return num == 0 ? 0 : ireg[num];
+	}
+
+	void setReg(uint32_t num, uint64_t val) override
+	{
+		if (num != 0)
+			ireg[num] = val;
+	}
+
+	void incPc(int64_t delta) override
+	{
+		pc_ += delta;
+	}
+
+	uint64_t getPc() const override
+	{
+		return pc_;
+	}
+
+private:
+	static constexpr uint32_t NUM_REGS = 32;
+	uint64_t pc_ = 0;
+	uint64_t ireg[NUM_REGS] = {0,};
+	// TODO memory
+};
+
+/// Interface to one architected instruction
 class Inst
 {
 public:
+	/// update 'state' for execution of this
 	virtual void execute(ArchState &state) const = 0;
+
+	///@return assembly string of this
 	virtual std::string disasm() const = 0;
 };
 
@@ -28,7 +75,8 @@ public:
 
 	void execute(ArchState &state) const override
 	{
-		// TODO
+		state.setReg(rd_, imm());
+		state.incPc(2);
 	}
 
 	std::string disasm() const override
@@ -78,6 +126,8 @@ Inst* decode32(uint32_t opc)
 
 int main(int argc, char **argv)
 {
+	SimpleArchState state;
+
 	// li a1, -4
 	// li a2, 1
 	// addw a2 = a2 + a1
@@ -86,33 +136,54 @@ int main(int argc, char **argv)
 		0x4605,
 		0x9e2d
 	};
-	uint32_t opcodes_sz = 3;
+	uint32_t opcodes_sz = 6;
 
-	uint64_t pc = 0; // pc[63:1]
-	for (; pc < opcodes_sz; ++pc)
+	for (; state.getPc() < opcodes_sz;)
 	{
-		const uint16_t opc = opcodes[pc];
+		const uint64_t pc = state.getPc();
+		const uint16_t opc = opcodes[pc >> 1];
+		uint32_t opc_sz = 2;
 
 		uint32_t full_inst = opc;
 		Inst *inst = nullptr;
 		if ((opc & 3) == 3)
 		{
 			// 32 bit instruction
-			++pc;
-			full_inst |= opcodes[pc] << 16;
+			opc_sz = 4;
+			full_inst |= opcodes[(pc+2) >> 1] << 16;
 
 			inst = decode32(full_inst);
 		}
 		else
 			inst = decode16(full_inst);
 
-		std::cout << std::hex << std::setw(8) << full_inst << std::dec << ' ';
+		std::cout << std::hex
+		          << std::setw(12) << pc << ' '
+		          << std::setw(8) << full_inst << ' '
+					 << std::dec;
 
 		if (!inst)
+		{
 			std::cout << "(null inst)";
+			state.incPc(opc_sz);
+		}
 		else
+		{
 			std::cout << inst->disasm();
+			inst->execute(state);
+		}
 
+		std::cout << std::endl;
+	}
+
+	std::cout << std::endl << "Architected State" << std::endl;
+	for (uint32_t i = 0; i < 32;)
+	{
+		for (uint32_t j = 0; j < 4; ++j, ++i)
+		{
+			std::cout << std::setw(2) << i << ' '
+			          << std::hex << std::setw(16) << state.getReg(i) << std::dec << ' ';
+		}
 		std::cout << std::endl;
 	}
 
